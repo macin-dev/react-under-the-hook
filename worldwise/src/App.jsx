5bd7dc00a936dea15e9e0cf3c5eb9ea744c3b978
macin <ernesto.dev24@gmail.com>
2024-10-28 20:46:01 -0600

    Styles modules / worldwise

--- worldwise/src/App.jsx
+++ worldwise/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
 import HomePage from "./pages/HomePage";
+import Login from "./pages/Login";
 import PageNotFound from "./pages/PageNotFound";
 
 const App = () => {
@@ -10,8 +11,10 @@ const App = () => {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
+        <Route path="product" element={<Product />} />
+        <Route path="login" element={<Login />} />
+
         <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
