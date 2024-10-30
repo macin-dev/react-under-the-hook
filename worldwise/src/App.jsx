dafd6fd824a61d52118e9469e784c61da75246f2
macin <ernesto.dev24@gmail.com>
2024-10-29 18:54:36 -0600

    use of server json package / worldwise

--- worldwise/src/App.jsx
+++ worldwise/src/App.jsx
@@ -5,8 +5,32 @@ import HomePage from "./pages/HomePage";
 import Login from "./pages/Login";
 import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./pages/AppLayout";
+import { useEffect, useState } from "react";
+import CityList from "./component/CityList";
+
+const BASE_URL_API = "http://localhost:9000";
 
 const App = () => {
+  const [cities, setCities] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        setIsLoading(true);
+        const resp = await fetch(`${BASE_URL_API}/cities`);
+        const data = await resp.json();
+        setCities(data);
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+
+    fetchData();
+  }, []);
+
   return (
     // Traditional Approach
     <BrowserRouter>
@@ -15,7 +39,17 @@ const App = () => {
         <Route path="pricing" element={<Pricing />} />
         <Route path="product" element={<Product />} />
         <Route path="login" element={<Login />} />
-        <Route path="app" element={<AppLayout />} />
+        <Route path="app" element={<AppLayout />}>
+          <Route
+            index
+            element={<CityList cities={cities} isLoading={isLoading} />}
+          />
+          <Route
+            path="cities"
+            element={<CityList cities={cities} isLoading={isLoading} />}
+          />
+          <Route path="countries" element={<p>List of countries</p>} />
+        </Route>
         <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
